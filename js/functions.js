6683b7441a934507f6900ae49e6ad198ee2ca268
Dzemal Maraneli <mao09@mail.ru>
2024-07-08 10:57:33 +0700

    Добавляет функцию расчета времени встречи.

--- js/functions.js
+++ js/functions.js
@@ -46,3 +46,27 @@ getNumber(2023);
 getNumber(-1);
 getNumber(1.5);
 getNumber('агент 007');
+
+function isTimeFits (dayStart, dayEnd, meetingStart, meetingTime) {
+  let result = false;
+
+  const dayStartSplited = dayStart.split(':');
+  const dayEndSplited = dayEnd.split(':');
+  const meetingStartSplited = meetingStart.split(':');
+
+  const dayStartTotalMinutes = (+dayStartSplited[0] * 60) + +dayStartSplited[1];
+  const dayEndTotalMinutes = (+dayEndSplited[0] * 60) + +dayEndSplited[1];
+  const meetingStartTotalMinutes = (+meetingStartSplited[0] * 60) + +meetingStartSplited[1];
+
+  if (meetingStartTotalMinutes >= dayStartTotalMinutes && (meetingStartTotalMinutes + meetingTime) <= dayEndTotalMinutes) {
+    result = true;
+  }
+
+  return result;
+}
+
+isTimeFits('08:00', '17:30', '14:00', 90);
+isTimeFits('8:0', '10:0', '8:0', 120);
+isTimeFits('08:00', '14:30', '14:00', 90);
+isTimeFits('14:00', '17:30', '08:0', 90);
+isTimeFits('8:00', '17:30', '08:00', 900);
