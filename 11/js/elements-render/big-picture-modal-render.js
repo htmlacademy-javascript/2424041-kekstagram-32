620af2439e8d7bf521cf16641136805281b2664b
Keks <keks@htmlacademy.ru>
2024-08-05 19:24:08 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/elements-render/big-picture-modal-render.js
@@ -0,0 +1,119 @@
+const bigPictureModal = document.querySelector('.big-picture');
+const bigPictureCloseButton = bigPictureModal.querySelector('.big-picture__cancel');
+const bigPictureImg = bigPictureModal.querySelector('.big-picture__img img');
+const bigPictureLikes = bigPictureModal.querySelector('.likes-count');
+const bigPictureDescription = bigPictureModal.querySelector('.social__caption');
+const bigPictureTotalCommentsCount = bigPictureModal.querySelector('.social__comment-total-count');
+const bigPictureCommentsList = document.querySelector('.social__comments');
+const bigPictureCommentsLoader = document.querySelector('.comments-loader');
+const commentTemplateItem = document.querySelector('#comment').content.querySelector('.social__comment');
+const bigPictureCommentsShownCount = document.querySelector('.social__comment-shown-count');
+
+const PART_OF_COMMENTS = 5;
+let comments = [];
+let renderedComments = 0;
+
+function escapeKeydown(evt) {
+  if (evt.key === 'Escape') {
+    bigPictureModalCloser();
+  }
+}
+
+function mouseButtonDown(evt) {
+  if (evt.target === bigPictureModal) {
+    bigPictureModalCloser();
+  }
+}
+
+function bigPictureCloseDown() {
+  bigPictureModalCloser();
+}
+
+function loadCommentsListener(evt) {
+  if (evt.target === bigPictureCommentsLoader) {
+    commentsPartRender(comments);
+  }
+}
+
+function bigPictureModalOpener() {
+  bigPictureModal.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+
+  document.addEventListener('keydown', escapeKeydown);
+  document.addEventListener('click', mouseButtonDown);
+  bigPictureCommentsLoader.addEventListener('click', loadCommentsListener);
+  bigPictureCloseButton.addEventListener('click', bigPictureCloseDown);
+}
+
+function bigPictureModalCloser() {
+  bigPictureModal.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+
+  document.removeEventListener('keydown', escapeKeydown);
+  document.removeEventListener('click', mouseButtonDown);
+  bigPictureCommentsLoader.removeEventListener('click', loadCommentsListener);
+  bigPictureCloseButton.removeEventListener('click', bigPictureCloseDown);
+
+  if (bigPictureCommentsLoader.classList.contains('hidden')) {
+    bigPictureCommentsLoader.classList.remove('hidden');
+  }
+
+  comments = [];
+  renderedComments = 0;
+}
+
+function commentRender(comment) {
+  const commentsItem = commentTemplateItem.cloneNode(true);
+
+  commentsItem.querySelector('.social__picture').src = comment.avatar;
+  commentsItem.querySelector('.social__picture').alt = comment.name;
+  commentsItem.querySelector('.social__text').textContent = comment.message;
+
+  return commentsItem;
+}
+
+function commentsPartRender(commentsArray) {
+  if(commentsArray) {
+    comments = commentsArray;
+  }
+
+  let commentsToRenderCount = Math.min(renderedComments + PART_OF_COMMENTS, comments.length);
+
+  if (commentsToRenderCount >= comments.length) {
+    commentsToRenderCount = comments.length;
+  }
+
+  const commentsFragment = document.createDocumentFragment();
+
+  for (let i = renderedComments; i < commentsToRenderCount; i++) {
+    const comment = commentRender(comments[i]);
+    commentsFragment.appendChild(comment);
+  }
+
+  bigPictureCommentsList.appendChild(commentsFragment);
+
+  renderedComments = commentsToRenderCount;
+
+  if (commentsToRenderCount >= comments.length) {
+    bigPictureCommentsLoader.classList.add('hidden');
+  } else {
+    bigPictureCommentsLoader.classList.remove('hidden');
+  }
+
+  bigPictureCommentsShownCount.textContent = renderedComments;
+}
+
+function bigPictureModalRender(item) {
+  bigPictureImg.src = item.url;
+  bigPictureLikes.textContent = item.likes;
+  bigPictureDescription.textContent = item.description;
+  bigPictureTotalCommentsCount.textContent = item.comments.length;
+
+  bigPictureModalOpener();
+
+  bigPictureCommentsList.replaceChildren();
+
+  commentsPartRender(item.comments);
+}
+
+export { bigPictureModalRender };
---
+++ 11/js/elements-render/image-upload-modal-render.js
@@ -0,0 +1,65 @@
+import { formValidation } from '../helpers/form-validation.js';
+
+const form = document.querySelector('.img-upload__form');
+
+const imageUploadModal = document.querySelector('.img-upload__overlay');
+const imageInput = document.querySelector('.img-upload__input');
+const imageEffectLevelInput = document.querySelector('.effect-level__value');
+const imageFormHashtagInput = document.querySelector('.text__hashtags');
+const imageFormDescriptionInput = document.querySelector('.text__description');
+const imageUploadCloseButton = document.querySelector('.img-upload__cancel');
+
+function escapeKeydown(evt) {
+  if (evt.key === 'Escape') {
+    if (document.activeElement === imageFormHashtagInput || document.activeElement === imageFormDescriptionInput) {
+      evt.stopPropagation();
+    } else {
+      imageUploadModalCloser();
+    }
+  }
+}
+
+function mouseButtonDown(evt) {
+  if (evt.target === imageUploadModal) {
+    imageUploadModalCloser();
+  }
+}
+
+function imageUploadCloseDown() {
+  imageUploadModalCloser();
+}
+
+function imageUploadModalOpener() {
+  imageUploadModal.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+
+  document.addEventListener('keydown', escapeKeydown);
+  document.addEventListener('click', mouseButtonDown);
+  imageUploadCloseButton.addEventListener('click', imageUploadCloseDown);
+
+  form.addEventListener('submit', (evt) => {
+    formValidation(evt);
+  });
+}
+
+function imageUploadModalCloser() {
+  imageUploadModal.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+
+  imageInput.value = '';
+  imageEffectLevelInput.value = '';
+  imageFormHashtagInput.value = '';
+  imageFormDescriptionInput.value = '';
+
+  document.removeEventListener('keydown', escapeKeydown);
+  document.removeEventListener('click', mouseButtonDown);
+  imageUploadCloseButton.removeEventListener('click', imageUploadCloseDown);
+
+  form.removeEventListener('submit', (evt) => {
+    formValidation(evt);
+  });
+}
+
+imageInput.addEventListener('change', () => {
+  imageUploadModalOpener();
+});
