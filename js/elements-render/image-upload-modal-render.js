97cd78c05713248544851bc54b0309264e5534bc
Dzemal Maraneli <mao09@mail.ru>
2024-08-07 22:16:12 +0700

    Добавлено масштабирование изображения и добавление фильтров

--- js/elements-render/image-upload-modal-render.js
+++ js/elements-render/image-upload-modal-render.js
@@ -1,12 +1,16 @@
 import { formValidation, pristine } from '../helpers/form-validation.js';
+import { resetImageSize, scaleSmallerClick, scaleBiggerClick } from '../helpers/image-resize.js';
+import { applyEffect, effectSlider } from '../helpers/image-filter.js';
 
 const form = document.querySelector('.img-upload__form');
-
 const imageUploadModal = document.querySelector('.img-upload__overlay');
 const imageInput = document.querySelector('.img-upload__input');
 const imageFormHashtagInput = document.querySelector('.text__hashtags');
 const imageFormDescriptionInput = document.querySelector('.text__description');
 const imageUploadCloseButton = document.querySelector('.img-upload__cancel');
+const scaleSmallerButton = document.querySelector('.scale__control--smaller');
+const scaleBiggerButton = document.querySelector('.scale__control--bigger');
+const filtersList = document.querySelector('.effects__list');
 
 function escapeKeydown(evt) {
   if (evt.key === 'Escape') {
@@ -18,12 +22,6 @@ function escapeKeydown(evt) {
   }
 }
 
-function mouseButtonDown(evt) {
-  if (evt.target === imageUploadModal) {
-    imageUploadModalCloser();
-  }
-}
-
 function imageUploadCloseDown() {
   imageUploadModalCloser();
 }
@@ -32,13 +30,23 @@ function formSubmitListener(evt) {
   formValidation(evt);
 }
 
+function filterListener(evt) {
+  const target = evt.target.closest('.effects__radio');
+  if (target) {
+    applyEffect(target);
+  }
+}
+
 function imageUploadModalOpener() {
   imageUploadModal.classList.remove('hidden');
   document.body.classList.add('modal-open');
 
   document.addEventListener('keydown', escapeKeydown);
-  document.addEventListener('click', mouseButtonDown);
   imageUploadCloseButton.addEventListener('click', imageUploadCloseDown);
+
+  scaleSmallerButton.addEventListener('click', scaleSmallerClick);
+  scaleBiggerButton.addEventListener('click', scaleBiggerClick);
+  filtersList.addEventListener('click', filterListener);
 
   form.addEventListener('submit', formSubmitListener);
 }
@@ -49,10 +57,15 @@ function imageUploadModalCloser() {
 
   pristine.reset();
   form.reset();
+  effectSlider.reset();
+  resetImageSize();
 
   document.removeEventListener('keydown', escapeKeydown);
-  document.removeEventListener('click', mouseButtonDown);
   imageUploadCloseButton.removeEventListener('click', imageUploadCloseDown);
+
+  scaleSmallerButton.removeEventListener('click', scaleSmallerClick);
+  scaleBiggerButton.removeEventListener('click', scaleBiggerClick);
+  filtersList.removeEventListener('click', filterListener);
 
   form.removeEventListener('submit', formSubmitListener);
 }
---
+++ js/helpers/image-filter.js
@@ -0,0 +1,111 @@
+const slider = document.querySelector('.effect-level__slider');
+const sliderContainer = document.querySelector('.img-upload__effect-level');
+const effectValue = document.querySelector('.effect-level__value');
+const upploadPreview = document.querySelector('.img-upload__preview img');
+
+const effects = {
+  'effect-chrome': {
+    options: {
+      range: {
+        min: 0,
+        max: 1,
+      },
+      start: 1,
+      step: 0.1
+    },
+    filter: 'grayscale',
+    unit: ''
+  },
+  'effect-sepia': {
+    options: {
+      range: {
+        min: 0,
+        max: 1,
+      },
+      start: 1,
+      step: 0.1
+    },
+    filter: 'sepia',
+    unit: ''
+  },
+  'effect-marvin': {
+    options: {
+      range: {
+        min: 0,
+        max: 100,
+      },
+      start: 100,
+      step: 1
+    },
+    filter: 'invert',
+    unit: '%'
+  },
+  'effect-phobos': {
+    options: {
+      range: {
+        min: 0,
+        max: 3,
+      },
+      start: 3,
+      step: 0.1
+    },
+    filter: 'blur',
+    unit: 'px'
+  },
+  'effect-heat': {
+    options: {
+      range: {
+        min: 1,
+        max: 3,
+      },
+      start: 3,
+      step: 0.1
+    },
+    filter: 'brightness',
+    unit: ''
+  }
+};
+
+let chosenEffect = 'effect-none';
+sliderContainer.classList.add('visually-hidden');
+
+const effectSlider = noUiSlider.create(slider, {
+  start: 100,
+  connect: 'lower',
+  range: {
+    'min': 0,
+    'max': 100
+  },
+  step: 0.1,
+  format: {
+    to: (value) => Number(value),
+    from: (value) => Number(value),
+  }
+});
+
+
+function applyEffect(item) {
+  chosenEffect = item.getAttribute('id');
+
+  if(chosenEffect === 'effect-none') {
+    sliderContainer.classList.add('visually-hidden');
+    effectSlider.reset();
+    upploadPreview.style.removeProperty('filter');
+  } else {
+    sliderContainer.classList.remove('visually-hidden');
+    effectSlider.updateOptions(effects[chosenEffect].options);
+  }
+}
+
+function updateEffect() {
+  const currentValue = effectSlider.get();
+  effectValue.value = currentValue;
+
+  if (chosenEffect !== 'effect-none') {
+    upploadPreview.style.filter = `${effects[chosenEffect].filter}(${currentValue}${effects[chosenEffect].unit})`;
+  }
+}
+
+effectSlider.on('update', updateEffect);
+
+export { applyEffect, effectSlider };
