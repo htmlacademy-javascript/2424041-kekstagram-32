594ae44e492bc6e6c73041e6fa3db8b7227edeb4
Dzemal Maraneli <mao09@mail.ru>
2024-08-09 00:18:30 +0700

    Теперь данные получаются с сервера и отправлются на сервер при загрузке фотографии.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import { galleryRender } from './elements-render/gallery-render.js';
 import './elements-render/image-upload-modal-render.js';
+import { getData } from './api.js';
 
-galleryRender();
+getData(galleryRender);
